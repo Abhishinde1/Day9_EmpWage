8e34caac72828d76995d6419fe87c2b25344794e
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-07 11:40:36 +0530

    Welcome to EmpWage

---
+++ Day9/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Day9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Empwage");
+            UC8.ComputeEmpWage("Barclays", 20, 20, 100);
+        }
+    }
+}
